0d3eec397ae10d39a8c30decf1ea1ca57099a3f6
Razvan <razvan@razshare.dev>
2025-08-14 03:28:23 +0200

    chore(app): bump.

--- astro.config.mjs
+++ astro.config.mjs
@@ -46,7 +46,6 @@ export default defineConfig({
             { label: "Docker", slug: "guides/docker" },
             { label: "Issues", slug: "guides/issues" },
             { label: "Contributing", slug: "guides/contributing" },
-            { label: "FAQ", slug: "guides/faq" },
           ],
         },
       ],
