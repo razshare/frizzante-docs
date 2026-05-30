28b6c94bd565a69d449233bda648f1d06faa0871
Razvan <razvan@razshare.dev>
2026-05-30 18:57:32 +0200

    chore(app): adding loading animation to pages.

--- app/lib/components/menu_item.svelte
+++ app/lib/components/menu_item.svelte
@@ -8,10 +8,28 @@
     .menu-item-shift {
         padding-left: calc(1rem * var(--shift));
     }
+    .menu-item-loading-indicator {
+        position: relative;
+        padding-left: 1rem;
+    }
+    .menu-item-loading-indicator-spinner {
+        position: absolute;
+        right: 0;
+        animation: spin 2s linear infinite;
+    }
+    @keyframes spin {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
 </style>
 
 <script lang="ts">
     import type { Snippet } from "svelte"
+    import { scale } from "svelte/transition"
     type Props = {
         children: Snippet
         loading?: boolean
@@ -24,7 +42,9 @@
     <div class="menu-item-shift" style:--shift={shift}>
         {@render children()}
         {#if loading}
-            <span>(loading...)</span>
+            <span in:scale class="menu-item-loading-indicator">
+                <span class="menu-item-loading-indicator-spinner">|</span>
+            </span>
         {/if}
     </div>
 </div>
